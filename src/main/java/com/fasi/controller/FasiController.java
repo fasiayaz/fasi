8374ab5bd8d3d5aa23783af5f905970bf591cbb2
baseerkhan034 <126024886+baseerkhan034@users.noreply.github.com>
2023-05-16 23:42:20 -0400

    Added new end point

--- src/main/java/com/fasi/controller/FasiController.java
+++ src/main/java/com/fasi/controller/FasiController.java
@@ -19,4 +19,10 @@ public class FasiController {
 		
 		return new ResponseEntity<String>("Hello "+name, HttpStatus.OK);
     }
+	
+	@GetMapping(value="/add", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> sayHello(@RequestParam Integer n1,@RequestParam Integer n2) {
+		
+		return new ResponseEntity<String>("Sum of "+n1+" and "+n2+" = "+(n1+n2), HttpStatus.OK);
+    }
 }
