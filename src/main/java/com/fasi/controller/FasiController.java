a32ef23cb93431fa720952213fcfe7ff22f54b60
fasiayaz <fasi.ayaz@gmail.com>
2023-05-16 22:56:29 -0400

    Testing new rest application in GIT.
    Testing new rest application in GIT.

---
+++ src/main/java/com/fasi/controller/FasiController.java
@@ -0,0 +1,22 @@
+package com.fasi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FasiController {
+
+	/*
+	 	http://localhost:8071/hello?name=Ravi
+	 	*/
+	
+	@GetMapping(value="/hello", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> sayHello(@RequestParam String name) {
+		
+		return new ResponseEntity<String>("Hello "+name, HttpStatus.OK);
+    }
+}
